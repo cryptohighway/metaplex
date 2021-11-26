a8b4d651226d03c5f13b632cda2b1b0c13887254
Mike Sperry <sales@sperrysoftware.com>
2021-11-26 14:59:29 -0500

    Improved layout and added a a basic Footer.

--- js/packages/web/src/components/Layout/index.tsx
+++ js/packages/web/src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from 'antd';
 
 import { AppBar } from '../AppBar';
+import { WavesImage } from '../WavesImage';
 import { Footer } from '../Footer';
 
 const { Header, Content } = Layout;
@@ -17,6 +18,7 @@ export const AppLayout = React.memo((props: any) => {
         <Header className="App-Bar">
           <AppBar />
         </Header>
+        <WavesImage />
         <Layout id={'width-layout'}>
           <Content
             style={{
@@ -24,10 +26,14 @@ export const AppLayout = React.memo((props: any) => {
               padding: '30px 48px ',
             }}
           >
+            <div className="directionsText">
+              <div className="directionsTitle">Purchase Data</div>
+              <div className="directionsSubTitle">Search the Tsunami marketplace to find then purchase the dataset or algorithm you need</div>
+            </div>
             {props.children}
           </Content>
         </Layout>
-        {/*<Footer />*/}
+        <Footer />
       </Layout>
     </>
   );
